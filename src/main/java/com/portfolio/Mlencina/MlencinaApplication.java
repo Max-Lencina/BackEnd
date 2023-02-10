8add45145d707247a3779962b170c9ae4c8ff65d
Max-Lencina <maxilencina1985@gmail.com>
2023-02-10 17:00:03 -0300

    primer commit

---
+++ src/main/java/com/portfolio/Mlencina/MlencinaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Mlencina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlencinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlencinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/Mlencina/MlencinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Mlencina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlencinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
