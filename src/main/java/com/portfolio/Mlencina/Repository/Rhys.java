138e468361ef5f713d21650bd4dd54ec1d7ac50a
Max-Lencina <maxilencina1985@gmail.com>
2023-02-19 14:39:26 -0300

    commit 4

---
+++ src/main/java/com/portfolio/Mlencina/Repository/Rhys.java
@@ -0,0 +1,12 @@
+
+package com.portfolio.Mlencina.Repository;
+
+import com.portfolio.Mlencina.Entity.hys;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface Rhys extends JpaRepository<hys, Integer>{
+    Optional<hys> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+}
